41db10e89b2fcfcac714046d9044c17395e44b0c
sethlinares <104448468+sethlinares@users.noreply.github.com>
2023-09-16 23:03:41 -0600

    W01 Prove

---
+++ Hello_World/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace cse310 {
+
+    public class Program {
+        public static void Main(String[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+}
+        
